fd26e155a31c5c2cf448beea7d231b942b3b61bb
Dom Estioko <dom.estioko.sg@gmail.com>
2022-10-04 05:57:48 +0800

    kata for the day

---
+++ 2022_10_04.js
@@ -0,0 +1,27 @@
+// DESCRIPTION
+
+// Convert a string to an array
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// TEST CASES
+
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
+
+// describe("Basic tests", () => {
+//   it("Testing for fixed tests", () => {
+//     assert.deepEqual(stringToArray("Robin Singh"), ["Robin", "Singh"]);
+//     assert.deepEqual(stringToArray("I love arrays they are my favorite"), ["I", "love", "arrays", "they", "are", "my", "favorite"]);
+//   });
+// });
+
+  
+// MY SOLUTION
+
+const stringToArray = string => string.split(" ");
